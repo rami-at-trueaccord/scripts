3d83ed78b8b05a7d1cab583bd7038e70717c5d8d
Rami <98785790+rami-at-trueaccord@users.noreply.github.com>
2023-04-12 09:52:17 -0500

    null: no change

--- aws/sandbox.py
+++ aws/sandbox.py
@@ -1,9 +1,9 @@
-#######################################################
+###################################################
 ##########  Boilerplate Relative Import Hack ##########
 import sys, os
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, parent_dir)
-#######################################################
+###################################################
 
 
 from general.aws_helpers.s3.buckets import get_object_encryption_status
